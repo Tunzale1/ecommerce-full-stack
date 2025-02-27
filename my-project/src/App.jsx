23bf0e2a1eb810498ffb40820b42b3c0ef423334
Tunzale1 <tunzaleimanova03@mail.ru>
2025-02-27 17:37:04 +0400

    first commit

---
+++ my-project/src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Header from './components/Header'
+import Home from './Pages/Home'
+
+function App() {
+
+  return (
+    <>
+    <BrowserRouter>
+    <Header/>
+    <Routes>
+    <Route path={"/"} exact={true} element={<Home/>} />
+    </Routes>
+    </BrowserRouter>
+    
+    </>
+  )
+}
+
+export default App
